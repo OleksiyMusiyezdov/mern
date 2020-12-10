f2304714283ff8cc0fce35289f15e53a2ff4935d
Apo-S <20630405+Apo-S@users.noreply.github.com>
2020-12-11 01:18:20 +0200

    small part of fixes

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express');
 const config = require('config');
-const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-//const mongoose = require('mongoose');
-const PORT = config.get('port') || 8080;
+const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
-app.use(express.json({ extended: true }));
+const PORT = config.get('port') || 8080;
+
+app.use(
+    cors(),
+    express.json(),
+    express.urlencoded({ extended: true }),
+);
 
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/mern_db';
@@ -21,37 +25,21 @@ MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (e
 
 /* Read */
 app.get('/api', (req, res) => {
-    console.log('GET-request is working.' + req);
-
     db.db().collection('notes').find().toArray((err, notes) => {
         if (err) throw err;
-        console.log(notes);
 
-        //res.send({ notes: notes });
-        return { notes: notes };
+        res.send({ notes: notes });
     });
 });
 
-/* addNewNote */
-app.get('/api/createNote'), (req, res) => {
+app.post('/api/createNote', (req, res) => {
     if (!req.body) return res.sendStatus(400);
-    var note = req.body;
-    var text = JSON.stringify(note);
-    console.log(`user UNINSERTED YET = ${text}`);
-};
 
-app.post('/api/createNote', urlencodedParser, (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    console.log("qwerty");
-
-    if (!req.body) return res.sendStatus(400);
-    var note = req.body;
-    // var text = JSON.stringify(note);
-    // console.log(`user UNINSERTED YET = ${text}`);
+    const note = req.body;
 
     db.db().collection('notes').insertOne(note, (err, result) => {
         if (err) throw err;
-        console.log("result.ops " + JSON.stringify(result.ops));
+        
         res.send({ result: result });
     });
 });
